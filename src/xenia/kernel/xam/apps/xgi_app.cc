377c4b61cd6cdf3b3c598970303515ed44f795cb
Codie Stella <git@codie.gg>
2023-03-22 21:46:12 +0000

    Async QOS, Dispatch Thread XNET

--- src/xenia/kernel/xam/apps/xgi_app.cc
+++ src/xenia/kernel/xam/apps/xgi_app.cc
@@ -304,9 +304,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         xe::be<uint16_t> dwMaxPrivateSlots;
       }* data = reinterpret_cast<message_data*>(buffer);
 
-      // Hacky fix for qos problem.
-      resetQosCache();
-
       XELOGI("XSessionModify({:08X} {:08X} {:08X} {:08X})",
              data->hSession.get(), data->dwFlags.get(),
              data->dwMaxPublicSlots.get(), data->dwMaxPrivateSlots.get());
@@ -382,9 +379,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       return X_E_SUCCESS;
     }
     case 0x000B001C: {
-      resetQosCache();
-
-
       XELOGI("XSessionSearchEx");
 
       int i = 0;
@@ -1101,7 +1095,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
     case 0x000B0006: {
       assert_true(!buffer_length ||
                   buffer_length == sizeof(XGI_XUSER_SET_CONTEXT));
-      resetQosCache();
       const XGI_XUSER_SET_CONTEXT* xgi_context =
           reinterpret_cast<const XGI_XUSER_SET_CONTEXT*>(buffer);
 
@@ -1181,8 +1174,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         // - XamSessionRefObjByHandle
         // - [this]
         // - CloseHandle
-        resetQosCache();
-
 
         uint32_t session_handle = xe::load_and_swap<uint32_t>(buffer + 0x0);
         uint32_t flags = xe::load_and_swap<uint32_t>(buffer + 0x4);
@@ -1302,7 +1293,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
     #pragma endregion
         } else {
     #pragma region Curl
-          resetQosCache();
 
             /*
                 TODO:
@@ -1414,9 +1404,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       XELOGD("XGISessionDelete({:08X}, {:08X}), implemented in netplay",
              buffer_ptr, buffer_length);
 
-            resetQosCache();
-
-
       struct message_data {
         xe::be<uint32_t> session_handle;
       }* data = reinterpret_cast<message_data*>(buffer);
@@ -1481,9 +1468,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       uint32_t xuid_array = xe::load_and_swap<uint32_t>(buffer + 0x8);
       uint32_t user_index_array = xe::load_and_swap<uint32_t>(buffer + 0xC);
       uint32_t private_slots_array = xe::load_and_swap<uint32_t>(buffer + 0x10);
-
-            resetQosCache();
-
 
       // Local uses user indices, remote uses XUIDs
       if (xuid_array == 0) {
@@ -1583,9 +1567,6 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       return X_E_SUCCESS;
     }
     case 0x000B0013: {
-      resetQosCache();
-
-
       assert_true(buffer_length == 0x14);
       uint32_t session_ptr = xe::load_and_swap<uint32_t>(buffer + 0x0);
       uint32_t array_count = xe::load_and_swap<uint32_t>(buffer + 0x4);
