7e3a62db5fe5e6d6ccb7aaff8f2c7294b6abe11a
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-06 16:16:19 +0000

    [XLiveAPI/APP] Improved XLiveAPI initialisation & Netplay toolbar tab
    Improved XLiveAPI initialisation
    Added Netplay toolbar tab

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -502,6 +502,7 @@ void EmulatorApp::OnDestroy() {
     xe::kernel::XLiveAPI::upnp_handler.~upnp();
   }
 
+  // Delete sessions on shutdown.
   xe::kernel::XLiveAPI::DeleteAllSessions();
 
   curl_global_cleanup();
