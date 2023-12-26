1503dd3dc1edde2250d0351f4c3cdad903ee1d44
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-26 13:59:05 +0000

    Mousehook (#12)

--- src/xenia/base/profiling.h
+++ src/xenia/base/profiling.h
@@ -17,10 +17,9 @@
 #include "xenia/base/platform.h"
 #include "xenia/base/string.h"
 #include "xenia/ui/ui_drawer.h"
-#include "xenia/ui/virtual_key.h"
 #include "xenia/ui/window_listener.h"
 
-#if XE_PLATFORM_WIN32 && 0
+#if XE_PLATFORM_WIN32
 #define XE_OPTION_PROFILING 1
 #define XE_OPTION_PROFILING_UI 1
 #else
@@ -204,13 +203,13 @@ class Profiler {
 #if XE_OPTION_PROFILING
   class ProfilerWindowInputListener final : public ui::WindowInputListener {
    public:
-    void OnKeyDown(ui::KeyEvent& e) override;
-    void OnKeyUp(ui::KeyEvent& e) override;
+    static void OnKeyDown(ui::KeyEvent& e, int key_code);
+    static void OnKeyUp(ui::KeyEvent& e, int key_code);
 #if XE_OPTION_PROFILING_UI
-    void OnMouseDown(ui::MouseEvent& e) override;
-    void OnMouseMove(ui::MouseEvent& e) override;
-    void OnMouseUp(ui::MouseEvent& e) override;
-    void OnMouseWheel(ui::MouseEvent& e) override;
+    static void OnMouseDown(ui::MouseEvent& e);
+    static void OnMouseMove(ui::MouseEvent& e);
+    static void OnMouseUp(ui::MouseEvent& e);
+    static void OnMouseWheel(ui::MouseEvent& e);
 #endif  // XE_OPTION_PROFILING_UI
   };
   // For now, no need for OnDpiChanged in a WindowListener because redrawing is
