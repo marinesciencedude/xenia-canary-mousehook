c750999f16a051d54de8fadb2493a65a3fa49333
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-26 13:59:05 +0000

    Mousehook (#12)
    followed by:
    Undo revert [UI/HID] ui::VirtualKey enum (#13)
    [HID] Cleanup Keyboard Input (#15) [Removed all mousehook specific code.]
    then:
    Restored mousehook code [Still need to restore scroll wheel and button combo bindings]
    [LINT] clang-format

--- src/xenia/ui/window_listener.h
+++ src/xenia/ui/window_listener.h
@@ -49,6 +49,9 @@ class WindowInputListener {
   virtual void OnMouseUp(MouseEvent& e) {}
   virtual void OnMouseWheel(MouseEvent& e) {}
 
+  virtual void OnRawMouse(MouseEvent& e) {}
+  virtual void OnRawKeyboard(KeyEvent& e) {}
+
   virtual void OnTouchEvent(TouchEvent& e) {}
 };
 
