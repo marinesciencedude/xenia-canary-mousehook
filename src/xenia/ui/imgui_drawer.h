2906a85eab73ca7713eecd254a7308b152d9308b
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-26 13:59:05 +0000

    Mousehook (#12)

--- src/xenia/ui/imgui_drawer.h
+++ src/xenia/ui/imgui_drawer.h
@@ -109,7 +109,6 @@ class ImGuiDrawer : public WindowInputListener, public UIDrawer {
   void RenderDrawLists(ImDrawData* data, UIDrawContext& ui_draw_context);
 
   void ClearInput();
-  void OnKey(KeyEvent& e, bool is_down);
   void UpdateMousePosition(float x, float y);
   void SwitchToPhysicalMouseAndUpdateMousePosition(const MouseEvent& e);
 
