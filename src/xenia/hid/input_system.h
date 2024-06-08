b33fe2fb5a5825d83c73650e8dc0064a0a0ee28d
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-06-08 13:58:22 +0100

    [HID] Cleanup Keyboard Input (#15)
    Removed all mousehook specific code.

--- src/xenia/hid/input_system.h
+++ src/xenia/hid/input_system.h
@@ -54,6 +54,8 @@ class InputSystem {
 
   uint32_t GetLastUsedSlot() const { return last_used_slot; }
 
+  void UpdateTitleId(uint32_t title_id);
+
   std::unique_lock<xe_unlikely_mutex> lock();
 
  private:
