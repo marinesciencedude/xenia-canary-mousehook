7d9a7b148d74b18f0a757236bc141e9e38f83203
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-06-13 21:18:01 +0100

    [LINT] clang-format

--- src/xenia/hid/winkey/hookables/SourceEngine.cc
+++ src/xenia/hid/winkey/hookables/SourceEngine.cc
@@ -59,56 +59,35 @@ struct GameBuildAddrs {
 
 // Replace with build names when we introduce more compatibility
 std::map<SourceEngine::GameBuild, GameBuildAddrs> supported_builds{
-    {
-        SourceEngine::GameBuild::CSGO, 
-        {kTitleIdCSGO, "5.0", 0x86955490, 0x4AE8}
-    },
-    {
-        SourceEngine::GameBuild::CSGO_Beta, 
-        {kTitleIdCSGO, "1.0.1.16", 0x8697DB30, 0x4AC8}
-    }, 
-    {
-        SourceEngine::GameBuild::L4D1, 
-        {kTitleIdL4D1, "1.0", 0x86536888, 0x4B44}
-    },
-    {
-        SourceEngine::GameBuild::L4D1_GOTY, 
-        {kTitleIdL4D1, "6.0", 0x86537FA0, 0x4B44}
-    },
-    {
-        SourceEngine::GameBuild::L4D2, 
-        {kTitleIdL4D2, "3.0", 0x86CC4E60, 0x4A94}
-    },
-    {
-        SourceEngine::GameBuild::OrangeBox, 
-        {kTitleIdOrangeBox, "4.0", NULL, 0x863F53A8}
-    },
-    {
-        SourceEngine::GameBuild::PortalSA,
-        {kTitleIdPortalSA, "3.0.1", NULL, 0x863F56B0}
-    },
-    {
-        SourceEngine::GameBuild::Portal2,
-        {kTitleIdPortal2, "4.0", 0x82C50180, 0x4A98}
-    },
-    {
-        SourceEngine::GameBuild::Portal2_TU1,
-        {kTitleIdPortal2, "4.0.1", 0x82C50220, 0x4A98}
-    }
-};
+    {SourceEngine::GameBuild::CSGO, {kTitleIdCSGO, "5.0", 0x86955490, 0x4AE8}},
+    {SourceEngine::GameBuild::CSGO_Beta,
+     {kTitleIdCSGO, "1.0.1.16", 0x8697DB30, 0x4AC8}},
+    {SourceEngine::GameBuild::L4D1, {kTitleIdL4D1, "1.0", 0x86536888, 0x4B44}},
+    {SourceEngine::GameBuild::L4D1_GOTY,
+     {kTitleIdL4D1, "6.0", 0x86537FA0, 0x4B44}},
+    {SourceEngine::GameBuild::L4D2, {kTitleIdL4D2, "3.0", 0x86CC4E60, 0x4A94}},
+    {SourceEngine::GameBuild::OrangeBox,
+     {kTitleIdOrangeBox, "4.0", NULL, 0x863F53A8}},
+    {SourceEngine::GameBuild::PortalSA,
+     {kTitleIdPortalSA, "3.0.1", NULL, 0x863F56B0}},
+    {SourceEngine::GameBuild::Portal2,
+     {kTitleIdPortal2, "4.0", 0x82C50180, 0x4A98}},
+    {SourceEngine::GameBuild::Portal2_TU1,
+     {kTitleIdPortal2, "4.0.1", 0x82C50220, 0x4A98}}};
 
 bool SourceEngine::IsGameSupported() {
   auto title_id = kernel_state()->title_id();
-  if (title_id != kTitleIdCSGO && title_id != kTitleIdL4D1 && title_id != kTitleIdL4D2 && title_id != kTitleIdOrangeBox && title_id != kTitleIdPortalSA && title_id != kTitleIdPortal2)
-      return false;
+  if (title_id != kTitleIdCSGO && title_id != kTitleIdL4D1 &&
+      title_id != kTitleIdL4D2 && title_id != kTitleIdOrangeBox &&
+      title_id != kTitleIdPortalSA && title_id != kTitleIdPortal2)
+    return false;
 
   const std::string current_version =
       kernel_state()->emulator()->title_version();
 
-  for (auto& build : supported_builds)
-  {
-    if (title_id == build.second.title_id && current_version == build.second.title_version)
-    {
+  for (auto& build : supported_builds) {
+    if (title_id == build.second.title_id &&
+        current_version == build.second.title_version) {
       game_build_ = build.first;
       return true;
     }
@@ -142,34 +121,31 @@ bool SourceEngine::DoHooks(uint32_t user_index, RawInputState& input_state,
   }
 
   uint32_t player_ptr;
-  if (supported_builds[game_build_].execute_addr)
-  {
-      current_thread->thread_state()->context()->r[3] = -1;
+  if (supported_builds[game_build_].execute_addr) {
+    current_thread->thread_state()->context()->r[3] = -1;
 
-      kernel_state()->processor()->Execute(
-            current_thread->thread_state(),
-            supported_builds[game_build_].execute_addr);
+    kernel_state()->processor()->Execute(
+        current_thread->thread_state(),
+        supported_builds[game_build_].execute_addr);
 
-      // Get player pointer
-      player_ptr =
-          static_cast<uint32_t>(current_thread->thread_state()->context()->r[3]);
+    // Get player pointer
+    player_ptr =
+        static_cast<uint32_t>(current_thread->thread_state()->context()->r[3]);
 
-      if (!player_ptr) {
-        // Not in game
-        return false;
-      }
+    if (!player_ptr) {
+      // Not in game
+      return false;
+    }
   }
 
   xe::be<uint32_t>* angle_offset;
 
-  if (supported_builds[game_build_].execute_addr)
-  {
+  if (supported_builds[game_build_].execute_addr) {
     angle_offset = kernel_memory()->TranslateVirtual<xe::be<uint32_t>*>(
         player_ptr + supported_builds[game_build_].angle_offset);
-  }
-  else 
-  {
-    angle_offset = kernel_memory()->TranslateVirtual<xe::be<uint32_t>*>(supported_builds[game_build_].angle_offset); 
+  } else {
+    angle_offset = kernel_memory()->TranslateVirtual<xe::be<uint32_t>*>(
+        supported_builds[game_build_].angle_offset);
   }
 
   QAngle* ang = reinterpret_cast<QAngle*>(angle_offset);
@@ -190,13 +166,10 @@ bool SourceEngine::DoHooks(uint32_t user_index, RawInputState& input_state,
     }
   }
 
-  if (!cvars::invert_x)
-  {
+  if (!cvars::invert_x) {
     camX -=
         (((float)input_state.mouse.x_delta) / 7.5f) * (float)cvars::sensitivity;
-  }
-  else
-  {
+  } else {
     camX +=
         (((float)input_state.mouse.x_delta) / 7.5f) * (float)cvars::sensitivity;
   }
@@ -216,15 +189,14 @@ bool SourceEngine::DoHooks(uint32_t user_index, RawInputState& input_state,
 }
 
 // probably making a mistake using a template, ah well
-template <typename T> int sgn(T val)
-{
+template <typename T>
+int sgn(T val) {
   return (T(0) < val) - (val < T(0));
 }
 
 bool SourceEngine::ModifierKeyHandler(uint32_t user_index,
-                                       RawInputState& input_state,
-                                       X_INPUT_STATE* out_state)
-{
+                                      RawInputState& input_state,
+                                      X_INPUT_STATE* out_state) {
   float thumb_lx = (int16_t)out_state->gamepad.thumb_lx;
   float thumb_ly = (int16_t)out_state->gamepad.thumb_ly;
 
@@ -240,28 +212,29 @@ bool SourceEngine::ModifierKeyHandler(uint32_t user_index,
   int multiplier_y = sgn(thumb_ly);
 
   // If angle DIV π⁄4 is odd
-  if (fmod(angle / 0.785398185f, 2) != 0)
-  { 
+  if (fmod(angle / 0.785398185f, 2) != 0) {
     distance_x = int16_t(cvars::walk_diagonal * multiplier_x);
     distance_y = int16_t(cvars::walk_diagonal * multiplier_y);
-  }
-  else 
-  {
-      // Default value equates to 134.99 h.u./s, any higher than this value and the movement speed immediately goes to max
-    distance_x = int16_t(cvars::walk_orthogonal * multiplier_x);  // Default value between SHRT_MAX * (177.4/255 and 177.5/255)
-    distance_y = int16_t(cvars::walk_orthogonal * multiplier_y);  // Default value between SHRT_MAX * (177.4/255 and 177.5/255)
+  } else {
+    // Default value equates to 134.99 h.u./s, any higher than this value and
+    // the movement speed immediately goes to max
+    distance_x = int16_t(cvars::walk_orthogonal *
+                         multiplier_x);  // Default value between SHRT_MAX *
+                                         // (177.4/255 and 177.5/255)
+    distance_y = int16_t(cvars::walk_orthogonal *
+                         multiplier_y);  // Default value between SHRT_MAX *
+                                         // (177.4/255 and 177.5/255)
   }
 
   // Need analogue-compatible version
-  //out_state->gamepad.thumb_lx = (int16_t)(distance * cosf(angle));
-  //out_state->gamepad.thumb_ly = (int16_t)(distance * sinf(angle));
+  // out_state->gamepad.thumb_lx = (int16_t)(distance * cosf(angle));
+  // out_state->gamepad.thumb_ly = (int16_t)(distance * sinf(angle));
   out_state->gamepad.thumb_lx = distance_x;
   out_state->gamepad.thumb_ly = distance_y;
 
-  // Return true to signal that we've handled the modifier, so default modifier won't be used
+  // Return true to signal that we've handled the modifier, so default modifier
+  // won't be used
   return true;
-
-
 }
 
 }  // namespace winkey
