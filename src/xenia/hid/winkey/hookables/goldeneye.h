98769018f9172e700955a72fdd1a8c8bd1fd46de
Marine Biologist <marine_science_dude@hotmail.co.uk>
2024-07-27 19:52:20 +0100

    Added support for mid-game key bindings changes
    Starting example is GoldenEye watch menu changing left mouse click to A and scroll wheel to D-Pad Up/Down

--- src/xenia/hid/winkey/hookables/goldeneye.h
+++ src/xenia/hid/winkey/hookables/goldeneye.h
@@ -37,6 +37,7 @@ class GoldeneyeGame : public HookableGame {
   bool IsGameSupported();
   bool DoHooks(uint32_t user_index, RawInputState& input_state,
                X_INPUT_STATE* out_state);
+  std::string ChooseBinds();
   bool ModifierKeyHandler(uint32_t user_index, RawInputState& input_state,
                           X_INPUT_STATE* out_state);
 
