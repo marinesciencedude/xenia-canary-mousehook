483d7f2fd80e6f75db8e1e9f5c95f4bda2fa0f76
Clippy95 <lockmare7@gmail.com>
2024-08-24 13:30:38 +0300

    Dead Rising test

---
+++ src/xenia/hid/winkey/hookables/DeadRising.cpp
@@ -0,0 +1,121 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2023 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#define _USE_MATH_DEFINES
+
+#include "xenia/hid/winkey/hookables/DeadRising.h"
+
+#include "xenia/base/platform_win.h"
+#include "xenia/cpu/processor.h"
+#include "xenia/emulator.h"
+#include "xenia/hid/hid_flags.h"
+#include "xenia/hid/input_system.h"
+#include "xenia/kernel/util/shim_utils.h"
+#include "xenia/kernel/xmodule.h"
+#include "xenia/kernel/xthread.h"
+#include "xenia/xbox.h"
+
+using namespace xe::kernel;
+
+DECLARE_double(sensitivity);
+DECLARE_bool(invert_y);
+DECLARE_bool(invert_x);
+
+const uint32_t kTitleIdDR2CZ = 0x58410A8D;
+
+namespace xe {
+namespace hid {
+namespace winkey {
+struct GameBuildAddrs {
+  uint32_t x_address;
+  uint32_t y_address;
+};
+
+std::map<DeadRisingGame::GameBuild, GameBuildAddrs> supported_builds{
+    {DeadRisingGame::GameBuild::Unknown, {NULL, NULL}},
+    {DeadRisingGame::GameBuild::DeadRising2_CaseZero,
+     {0x8273BAFC, 0xEA4D238C}}};
+
+DeadRisingGame::~DeadRisingGame() = default;
+
+bool DeadRisingGame::IsGameSupported() {
+
+  return true;
+}
+
+float DeadRisingGame::DegreetoRadians(float degree) {
+  return (float)(degree * (M_PI / 180));
+}
+
+float DeadRisingGame::RadianstoDegree(float radians) {
+  return (float)(radians * (180 / M_PI));
+}
+
+bool DeadRisingGame::DoHooks(uint32_t user_index, RawInputState& input_state,
+                            X_INPUT_STATE* out_state) {
+  if (!IsGameSupported()) {
+    return false;
+  }
+
+  //if (supported_builds.count(game_build_) == 0) {
+  //  return false;
+ // }
+
+  // Don't constantly write if there is no mouse movement.
+
+  XThread* current_thread = XThread::GetCurrentThread();
+
+  if (!current_thread) {
+    return false;
+  }
+
+  xe::be<float>* radian_x = kernel_memory()->TranslateVirtual<xe::be<float>*>(
+      supported_builds[game_build_].x_address);
+
+  xe::be<float>* radian_y = kernel_memory()->TranslateVirtual<xe::be<float>*>(
+      supported_builds[game_build_].y_address);
+
+/* if (!radian_x || *radian_x == NULL) {
+    // Not in game
+    return false;
+  }
+  */
+  float degree_x = RadianstoDegree(*radian_x);
+  float degree_y = RadianstoDegree(*radian_y);
+
+  // X-axis = 0 to 360
+  if (!cvars::invert_x) {
+    degree_x += (input_state.mouse.x_delta / 5.f) * (float)cvars::sensitivity;
+  } else {
+    degree_x -= (input_state.mouse.x_delta / 5.f) * (float)cvars::sensitivity;
+  }
+
+  *radian_x = DegreetoRadians(degree_x);
+
+  if (!cvars::invert_y) {
+    degree_y += (input_state.mouse.y_delta / 5.f) * (float)cvars::sensitivity;
+  } else {
+    degree_y -= (input_state.mouse.y_delta / 5.f) * (float)cvars::sensitivity;
+  }
+
+  *radian_y = DegreetoRadians(degree_y);
+
+  return true;
+}
+
+std::string DeadRisingGame::ChooseBinds() { return "Default"; }
+
+bool DeadRisingGame::ModifierKeyHandler(uint32_t user_index,
+                                       RawInputState& input_state,
+                                       X_INPUT_STATE* out_state) {
+  return false;
+}
+}  // namespace winkey
+}  // namespace hid
+}  // namespace xe
