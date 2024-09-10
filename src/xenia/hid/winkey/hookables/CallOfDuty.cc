c5efc585c50c5253c6dc76af4590a71b9b124c9f
Clippy95 <lockmare7@gmail.com>
2024-09-10 15:35:52 +0300

    Ghosts May 08 build default.xex (untested and no fovscale)

--- src/xenia/hid/winkey/hookables/CallOfDuty.cc
+++ src/xenia/hid/winkey/hookables/CallOfDuty.cc
@@ -139,7 +139,10 @@ std::map<CallOfDutyGame::GameBuild, GameBuildAddrs> supported_builds{
       0x82FC6708, 0x82832AC8}},
     {CallOfDutyGame::GameBuild::CallOfDutyGhosts_IW6_DEV_1iw6sp,
      {0x82032648, 0x63675F66, kTitleIdCODGhostsDEV, 0x60, NULL, 0x84AF5CCC,
-      0x82D81130, NULL}}};
+      0x82D81130, NULL}},
+    {CallOfDutyGame::GameBuild::CallOfDutyGhosts_IW6_DEV_1iw6sp,
+     {0x82021FC4, 0x63675F66, kTitleIdCODGhostsDEV, 0x60, NULL, 0x84AF5CCC,
+      0x82956468, NULL}}};
 
 CallOfDutyGame::~CallOfDutyGame() = default;
 
