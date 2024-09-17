4757f9d6b7fcaf179e8fd66ed450127240569df0
Clippy95 <lockmare7@gmail.com>
2024-09-17 17:00:01 +0300

    Add extra auto center address to nop

--- src/xenia/emulator.cc
+++ src/xenia/emulator.cc
@@ -1596,17 +1596,18 @@ X_STATUS Emulator::CompleteLaunch(const std::filesystem::path& path,
                                           // on foot/horse if the in-game auto
                                           // center option is disabled.
       uint32_t aim_assist_function_address;
+      uint32_t alt_auto_center_read_address;  // Most likely either horz or vert
     };
     std::vector<RDRPatchOffsets> supported_builds = {
         // RDR GOTY DISK 1
-        {0x82010BEC, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82371E78,
-         0x822F9E60},
+        {0x82010BEC, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82371E78, 0x822F9E60,
+         0x82371E70},
         // RDR TU0
-        {0x8201071C, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82370C08,
-         0x822F83B0},
+        {0x8201071C, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82370C08, 0x822F83B0,
+         0x82370C28},
         // RDR GOTY DISK 2
-        {0x82010C0C, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82371E58,
-         0x822F9F60}};
+        {0x82010C0C, 0x7A3A5C72, 0x60000000, 0x4e800020, 0x82371E58, 0x822F9F60,
+         0x82371E3C}};
     for (auto& build : supported_builds) {
       auto* test_addr = (xe::be<uint32_t>*)module->memory()->TranslateVirtual(
           build.check_addr);
@@ -1614,6 +1615,7 @@ X_STATUS Emulator::CompleteLaunch(const std::filesystem::path& path,
         continue;
       }
       patch_addr(build.auto_center_read_address, build.BENop);
+      patch_addr(build.alt_auto_center_read_address, build.BENop);
       if (cvars::disable_autoaim && build.aim_assist_function_address) {
         patch_addr(build.aim_assist_function_address, build.BEStub);
       }
