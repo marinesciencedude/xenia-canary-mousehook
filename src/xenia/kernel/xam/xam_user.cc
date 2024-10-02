OpenCombas/xenia-canary

Netplay proof-of-concept.

src/xenia/kernel/xam/xam_user.cc
@@ -18,6 +18,7 @@
 #include "xenia/kernel/xam/xam_private.h"
 #include "xenia/kernel/xenumerator.h"
 #include "xenia/xbox.h"
+#include <random>
 
 #include "third_party/stb/stb_image.h"
 
@@ -129,6 +130,9 @@ X_HRESULT_result_t XamUserGetSigninInfo_entry(
   if (!flags || flags & X_USER_GET_SIGNIN_INFO_OFFLINE_XUID_ONLY) {
     info->xuid = user_profile->xuid();
   }
+
+  // This flag seems to tell the title we're online.
+  info->flags = 1;
 
   info->signin_state = user_profile->signin_state();
   return X_E_SUCCESS;
@@ -433,6 +437,13 @@ dword_result_t XamUserCheckPrivilege_entry(dword_t user_index, dword_t mask,
 
   // If we deny everything, games should hopefully not try to do stuff.
   *out_value = 0;
+
+  const auto& user_profile = kernel_state()->xam_state()->GetUserProfile(user_index);
+  if (user_profile->signin_state() == 2) {
+    // We have enabled Live so let's allow multiplayer
+    *out_value = 1;
+  }
+
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamUserCheckPrivilege, kUserProfiles, kStub);
@@ -977,17 +988,31 @@ dword_result_t XamWriteGamerTile_entry(
 DECLARE_XAM_EXPORT1(XamWriteGamerTile, kUserProfiles, kSketchy);
 
 dword_result_t XamSessionCreateHandle_entry(lpdword_t handle_ptr) {
-  *handle_ptr = 0xCAFEDEAD;
+  std::random_device rd;
+  std::uniform_int_distribution<uint32_t> dist(0, 0xFFFFFFFF);
+  *handle_ptr = dist(rd);
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamSessionCreateHandle, kUserProfiles, kStub);
 
+// This is dirty, we should be creating a local xobject for sessions,
+// but I've not implemented that, so instead I pass the session handle around
+// as if it's a pointer. Fortunately all functions which use that pointer are
+// within xenia anyway, but in future this should actually be a pointer.
+// - Codie
+
+//dword_result_t XamSessionRefObjByHandle_entry(dword_t handle,
+//                                              lpdword_t obj_ptr) {
+//  assert_true(handle == 0xCAFEDEAD);
+//  // TODO(PermaNull): Implement this properly,
+//  // For the time being returning 0xDEADF00D will prevent crashing.
+//  *obj_ptr = 0xDEADF00D;
+//  return X_ERROR_SUCCESS;
+//}
+
 dword_result_t XamSessionRefObjByHandle_entry(dword_t handle,
                                               lpdword_t obj_ptr) {
-  assert_true(handle == 0xCAFEDEAD);
-  // TODO(PermaNull): Implement this properly,
-  // For the time being returning 0xDEADF00D will prevent crashing.
-  *obj_ptr = 0xDEADF00D;
+  *obj_ptr = (uint32_t)handle;
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamSessionRefObjByHandle, kUserProfiles, kStub);

marinesciencedude/xenia-canary-mousehook

src/xenia/kernel/xam/xam_user.cc
@@ -19,6 +19,7 @@
 #include "xenia/kernel/xenumerator.h"
 #include "xenia/kernel/xthread.h"
 #include "xenia/xbox.h"
+#include <random>
 
 DECLARE_int32(user_language);
 
@@ -104,6 +105,8 @@ X_HRESULT_result_t XamUserGetSigninInfo_entry(
     info->signin_state = user_profile->signin_state();
     xe::string_util::copy_truncating(info->name, user_profile->name(),
                                      xe::countof(info->name));
+    // This flag seems to tell the title we're online.
+    info->unk08 = 1;
   } else {
     return X_E_NO_SUCH_USER;
   }
@@ -428,7 +431,7 @@ dword_result_t XamUserWriteProfileSettings_entry(
 }
 DECLARE_XAM_EXPORT1(XamUserWriteProfileSettings, kUserProfiles, kImplemented);
 
-dword_result_t XamUserCheckPrivilege_entry(dword_t user_index, dword_t mask,
+dword_result_t XamUserCheckPrivilege_entry(dword_t user_index, dword_t type,
                                            lpdword_t out_value) {
   // checking all users?
   if (user_index != 0xFF) {
@@ -443,6 +446,13 @@ dword_result_t XamUserCheckPrivilege_entry(dword_t user_index, dword_t mask,
 
   // If we deny everything, games should hopefully not try to do stuff.
   *out_value = 0;
+
+  const auto& user_profile = kernel_state()->xam_state()->GetUserProfile(user_index);
+  if (user_profile->signin_state() == 2) {
+    // We have enabled Live so let's allow multiplayer
+    *out_value = 1;
+  }
+
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamUserCheckPrivilege, kUserProfiles, kStub);
@@ -694,17 +704,31 @@ dword_result_t XamWriteGamerTile_entry(dword_t arg1, dword_t arg2, dword_t arg3,
 DECLARE_XAM_EXPORT1(XamWriteGamerTile, kUserProfiles, kStub);
 
 dword_result_t XamSessionCreateHandle_entry(lpdword_t handle_ptr) {
-  *handle_ptr = 0xCAFEDEAD;
+  std::random_device rd;
+  std::uniform_int_distribution<uint32_t> dist(0, 0xFFFFFFFF);
+  *handle_ptr = dist(rd);
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamSessionCreateHandle, kUserProfiles, kStub);
 
+// This is dirty, we should be creating a local xobject for sessions,
+// but I've not implemented that, so instead I pass the session handle around
+// as if it's a pointer. Fortunately all functions which use that pointer are
+// within xenia anyway, but in future this should actually be a pointer.
+// - Codie
+
+//dword_result_t XamSessionRefObjByHandle_entry(dword_t handle,
+//                                              lpdword_t obj_ptr) {
+//  assert_true(handle == 0xCAFEDEAD);
+//  // TODO(PermaNull): Implement this properly,
+//  // For the time being returning 0xDEADF00D will prevent crashing.
+//  *obj_ptr = 0xDEADF00D;
+//  return X_ERROR_SUCCESS;
+//}
+
 dword_result_t XamSessionRefObjByHandle_entry(dword_t handle,
                                               lpdword_t obj_ptr) {
-  assert_true(handle == 0xCAFEDEAD);
-  // TODO(PermaNull): Implement this properly,
-  // For the time being returning 0xDEADF00D will prevent crashing.
-  *obj_ptr = 0xDEADF00D;
+  *obj_ptr = (uint32_t)handle;
   return X_ERROR_SUCCESS;
 }
 DECLARE_XAM_EXPORT1(XamSessionRefObjByHandle, kUserProfiles, kStub);