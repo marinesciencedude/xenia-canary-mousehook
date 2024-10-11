85104b199469812159c3a9da54a606fb791742ef
Clippy95 <qpzezoqp15@gmail.com>
2024-10-11 18:54:40 +0300

    Red Dead Redemption Support (#29)
    * Basic support for Red Dead Redemption GOTY Disk 1
    
    * Support for Red Dead Redemption Normal(OG) TU0
    
    * Fix auto centering, included address for GOTY D1
    
    * Fix mount centering, provided address for GOTY D1
    
    * Added RDR GOTY Disk 2 support.
    Make finding addresses faster, will find auto_center_strength if we have the x address
    
    * Fix sens
    
    * Provide pointer for camera base
    
    * Add cover mousehook, (GOTY DISK 1)
    
    * Add mounted guns mouse hook (Address for GOTY 1)
    
    * Implement Carriage mousehook
    
    * TU0 Parity with GOTY
    
    * Initial bindings
    
    * cover base address, pause flag, TU0 fix?
    
    * Pointer for cam type, GOTY Disk 1 + TU0
    
    * Pointer for cam type, and ADS fovscale.
    
    * Aim Assist / Auto-center removal for GOTY Disk 1 and TU0
    
    * Basic Weapon Wheel support for GOTY Disk 1
    
    * Fix mounted guns X axis address, static FOV address.
    and unify the GOTY 1 & 2 addresses, Disk 1, UN (no mounted yet), and Reality Redemption should work.
    
    * GOTY Disk 2 mounted gun address
    
    * Lint
    
    * Comment equivalents to @DottierGalaxy50 cheat engine table.
    comment out printf for FOV
    
    * Auto center / Aim Assist removal for GOTY Disk 2
    
    * lint again
    
    * Fix Gatling gun not being detected
    
    * Auto center cover address (it's offseted from mounting_center_address)
    
    * Fovscale address for TU0
    
    * Turret cam for TU0
    
    * Weapon wheel base addresses, correct turret cam address for TU0
    
    * Add extra auto center address to nop
    
    * fovscale;turret;weapon wheel address: TU0; linted
    
    * correct alt auto center
    
    * RDR TU9 support
    
    * RDR Undead Nightmare standalone TU4
    Correct cover address / offsets for TU9
    
    * Replace MAP with struct in attempt to fix crashes at runtime.
    
    * lint sturct
    
    * Different weapon wheel logic
    
    * Snappy wheel test
    
    * IsWeaponWheelShown function, and stop camera from moving while in cover
    
    * lint
    
    * CinematicType addresses and handleRigthStick emulation for duels and horseshoe minigame
    
    * Limit Y axis correctly as game doesn't handle it
    
    * Fix camera not moving while in a carriage as a passenger and let the right stick move in a duel
    
    * GetCamType function, fixes crashes.
    
    * FOV sensitivity
    
    * IsPaused function
    
    * Turbo sprint
    
    * Option to enable turbo gallop for horses/coaches
    
    * Sanity check for mounting horses auto center
    
    * pattern scan test
    
    * cover sanity and lower start_address
    
    * sanity check for cover auto center
    
    * Fix carriage camera locking
    
    * Sanity check for carriage and a pattern scan if it's invalid.
    
    * Pattern scan horse auto center if sanity fails
    
    * Invert X and Invert Y, and refix pessanger carriage / cleanup for cover / mounted guns
    
    * lint
    
    * fix lint breaking
    
    * Bindings by @A1eNaz
    
    * Remove alt auto center address for Disk 2 as it required protect_zero=false
    
    * Expand binding
    
    * numbers as dpad
    
    * Add snappy wheel as a toggle, and rename config options to point they're for RDR
    
    * RDR in readme
    
    * Mid game bindings for horse
    
    * Print *what* pattern it finds in the console
    
    * Change pointer for disk 2 cover camera
    
    * Fovscale pointer
    
    * Allow RS binds to be used for weapon wheel
    
    * Better check for if mouse is moving.
    
    * lint mouse check
    
    * Change variable names
    
    ---------
    
    Co-authored-by: Clippy95 <lockmare7@gmail.com>
    Co-authored-by: Marine Biologist <marine_science_dude@hotmail.co.uk>

---
+++ src/xenia/hid/winkey/hookables/RDR.h
@@ -0,0 +1,79 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2023 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_HID_WINKEY_RedDeadRedemption_H_
+#define XENIA_HID_WINKEY_RedDeadRedemption_H_
+
+#include "xenia/base/chrono.h"
+#include "xenia/hid/winkey/hookables/hookable_game.h"
+
+namespace xe {
+namespace hid {
+namespace winkey {
+
+class RedDeadRedemptionGame : public HookableGame {
+ public:
+  enum /* class*/ GameBuild {
+    Unknown,
+    RedDeadRedemption_GOTY_Disk1,
+    RedDeadRedemption_GOTY_Disk2,
+    RedDeadRedemption_Original_TU0,
+    RedDeadRedemption_Original_TU9,
+    RedDeadRedemption_UndeadNightmare_Standalone_TU4
+  };
+
+  ~RedDeadRedemptionGame() override;
+
+  bool IsGameSupported();
+
+  float RadianstoDegree(float radians);
+  float DegreetoRadians(float degree);
+  bool IsWeaponWheelShown();
+  void HandleWeaponWheelEmulation(RawInputState& input_state,
+                                  X_INPUT_STATE* out_state);
+  bool IsCinematicTypeEnabled();
+  bool IsPaused();
+  void HandleRightStickEmulation(RawInputState& input_state,
+                                 X_INPUT_STATE* out_state);
+  float ClampVerticalAngle(float degree_y);
+  uint8_t GetCamType();
+
+  uint32_t FindPatternWithWildcardAddress(uint32_t start_address,
+                                          uint32_t end_address,
+                                          const std::vector<uint8_t>& pattern,
+                                          const char* pattern_name);
+
+  bool CompareMemoryWithPattern(const uint8_t* memory,
+                                const std::vector<uint8_t>& pattern);
+
+  bool DoHooks(uint32_t user_index, RawInputState& input_state,
+               X_INPUT_STATE* out_state);
+
+  std::string ChooseBinds();
+
+  bool IsMouseMoving(const RawInputState& input_state);
+
+  bool ModifierKeyHandler(uint32_t user_index, RawInputState& input_state,
+                          X_INPUT_STATE* out_state);
+
+ private:
+  GameBuild game_build_ = GameBuild::Unknown;
+  std::chrono::steady_clock::time_point last_movement_time_x_;
+  std::chrono::steady_clock::time_point last_movement_time_y_;
+  static uint32_t cached_carriage_x_address;
+  static uint32_t cached_carriage_y_address;
+  static uint32_t cached_carriage_z_address;
+  static uint32_t cached_auto_center_strength_address_carriage;
+};
+
+}  // namespace winkey
+}  // namespace hid
+}  // namespace xe
+
+#endif  // XENIA_HID_WINKEY_RedDeadRedemption_H_
