14453b5f4035adc4e21eae2fde170afd3bc45a86
Clippy95 <qpzezoqp15@gmail.com>
2024-11-06 22:01:38 +0300

    Saints Row 1 support & additions to Saints Row 2 (#39)
    * SR1 fixup.
    Patches to allow mouse in-game to work, temporary RS sens fix, / X-axis addition over frametime as the game does so, this fixes sens changing with framerate.
    
    * Havok frametime fix for SR1&2, rename SaintsRow to SaintsRow2
    
    * Pause flag, fix menu & vehicle flag addreses, should fix missions.
    
    * bindings
    
    * Add support for @Tervel1337 SR1 fine aim plugin
    
    * Don't hog GetModule path for fine aim plugin & default havok frametime fix to false
    
    * Slowdown slow_pan_horizontal_multiplier & slow_pan_vertical_multiplier_address
    
    * Update both normal Y axis and unused fine aim Y axis.
    This isn't done in the original game as it seems to be unfinished but this is much more preferable.
    
    * isPaused flag, enhances plugin check.
    
    * Improve sens/slow pan multiplier patch
    
    * Better drive cam patch for Saints Row 1
    
    * Port Hold Fine Aim for Saints Row 2
    https://github.com/Clippy95/RowInput/blob/8f2dd873c2bccd6d48b7b868cabc36297587f13a/addons/HoldFineAIM.cpp
    also nop havok write address for SR2
    
    * Lint havok fix
    
    * Patch to increase vehicle rotation limit for Saints Row 1
    
    * Always apply FOV for SR2
    
    * sniper zoom function address, used to holdfineaim for snipers, since the zoom seems to be different from normal fineaim.
    
    * Sensible max limit for sr1_increase_vehicle_rotation_limit.
    
    * bindings: rename Saints Row to Saints Row 1
    
    ---------
    
    Co-authored-by: Clippy95 <lockmare7@gmail.com>

---
+++ src/xenia/hid/winkey/hookables/SaintsRow2.h
@@ -0,0 +1,58 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2023 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_HID_WINKEY_SaintsRow_H_
+#define XENIA_HID_WINKEY_SaintsRow_H_
+
+#include <chrono>  // Include for chrono timing
+#include "xenia/hid/winkey/hookables/hookable_game.h"
+
+namespace xe {
+namespace hid {
+namespace winkey {
+
+class SaintsRow2Game : public HookableGame {
+ public:
+  enum class GameBuild { Unknown, SaintsRow2_TU3 };
+
+  ~SaintsRow2Game() override;
+
+  bool IsGameSupported();
+
+  float RadianstoDegree(float radians);
+  float DegreetoRadians(float degree);
+
+  bool DoHooks(uint32_t user_index, RawInputState& input_state,
+               X_INPUT_STATE* out_state);
+
+  std::string ChooseBinds();
+
+  bool ModifierKeyHandler(uint32_t user_index, RawInputState& input_state,
+                          X_INPUT_STATE* out_state);
+
+  void FixHavokFrameTime();
+
+  uint64_t reset_fineaim(uint32_t function_address, uint32_t player_ptr,
+                         uint32_t a2, uint32_t a3);
+
+ private:
+  GameBuild game_build_ = GameBuild::Unknown;
+
+  // Timer variables to hold the state for a while // this is probably not ideal
+  // -Clippy95
+  std::chrono::steady_clock::time_point last_movement_time_x_;
+  std::chrono::steady_clock::time_point last_movement_time_y_;
+  uint32_t player_status;
+};
+
+}  // namespace winkey
+}  // namespace hid
+}  // namespace xe
+
+#endif  // XENIA_HID_WINKEY_SaintsRow_H_
