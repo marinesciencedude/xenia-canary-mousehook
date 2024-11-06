7ba21307e77baa385a85be67d0cfb53f41d3c017
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-11-06 11:41:56 +0000

    [Kernel] Network mode selector

--- src/xenia/kernel/kernel_state.cc
+++ src/xenia/kernel/kernel_state.cc
@@ -39,7 +39,7 @@ DEFINE_uint32(kernel_build_version, 1888, "Define current kernel version",
 
 DECLARE_string(cl);
 
-DECLARE_bool(offline_mode);
+DECLARE_int32(network_mode);
 
 namespace xe {
 namespace kernel {
@@ -892,10 +892,13 @@ void KernelState::RegisterNotifyListener(XNotifyListener* listener) {
   }
 
   if (listener->mask() & kXNotifyLive) {
-    uint32_t live_connection_state =
-        cvars::offline_mode ? X_ONLINE_S_LOGON_DISCONNECTED
-                            : X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED;
-    uint32_t ethernet_link_state = cvars::offline_mode ? 0 : 1;
+    const uint32_t live_connection_state =
+        cvars::network_mode == NETWORK_MODE::XBOXLIVE
+            ? X_ONLINE_S_LOGON_CONNECTION_ESTABLISHED
+            : X_ONLINE_S_LOGON_DISCONNECTED;
+
+    const uint32_t ethernet_link_state =
+        cvars::network_mode == NETWORK_MODE::OFFLINE ? 0 : 1;
 
     listener->EnqueueNotification(kXNotificationLiveConnectionChanged,
                                   live_connection_state);
