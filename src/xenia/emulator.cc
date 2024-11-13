13af307efc9eaa9a33f93debeeb1539157fd22c5
Marine Biologist <marine_science_dude@hotmail.co.uk>
2024-11-13 19:30:57 +0000

    Minecraft TU75 support
    New address set-up, hotbar selection, pause flag found by Clippy95. Hotbar can now be instantly switched to the relevant position with the number keys - this new functionality can be used for other games in future. Inventory-specific bindings have been added - B button has been left at Q for now, shift without click is Y, distribute items (R) cannot currently be set to mouse buttons, E cannot currently be set to close interfaces. Inventory cursor no longer snaps onto item slots.
    
    Co-authored-by: Clippy95 <qpzezoqp15@gmail.com>

--- src/xenia/emulator.cc
+++ src/xenia/emulator.cc
@@ -2243,6 +2243,17 @@ X_STATUS Emulator::CompleteLaunch(const std::filesystem::path& path,
     }
   }
 
+  if (module->title_id() == 0x584111F7) {  // Minecraft - Prevent game from
+                                           // writing to inventory cursor
+    std::map<std::string, uint32_t> supported_builds = {{"1.0.80", 0x827594EC}};
+    for (auto& build : supported_builds) {
+      if (build.first == title_version_) {
+        patch_addr(build.second, 0x60000000);
+        break;
+      }
+    }
+  }
+
   // Initialize shader storage asynchronously - pipeline compilation happens in
   // background while the game goes through its normal startup (loading screens,
   // intro videos, etc.). With async_shader_compilation enabled, draws are
