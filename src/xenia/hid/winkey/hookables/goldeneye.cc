b466d209ba3a3b4923dc9d16e3b00487d8ec8f36
Marine Biologist <marine_science_dude@hotmail.co.uk>
2024-11-13 19:30:57 +0000

    Minecraft TU75 support
    New address set-up, hotbar selection, pause flag found by Clippy95. Hotbar can now be instantly switched to the relevant position with the number keys - this new functionality can be used for other games in future. Inventory-specific bindings have been added - B button has been left at Q for now, shift without click is Y, distribute items (R) cannot currently be set to mouse buttons, E cannot currently be set to close interfaces. Inventory cursor no longer snaps onto item slots.
    
    Co-authored-by: Clippy95 <qpzezoqp15@gmail.com>

--- src/xenia/hid/winkey/hookables/goldeneye.cc
+++ src/xenia/hid/winkey/hookables/goldeneye.cc
@@ -574,6 +574,11 @@ bool GoldeneyeGame::ModifierKeyHandler(uint32_t user_index,
   return true;
 }
 
+void GoldeneyeGame::WeaponSwitchHandler(uint32_t user_index,
+                                        RawInputState& input_state,
+                                        X_INPUT_STATE* out_state, int weapon,
+                                        uint16_t buttons) {}
+
 }  // namespace winkey
 }  // namespace hid
 }  // namespace xe
