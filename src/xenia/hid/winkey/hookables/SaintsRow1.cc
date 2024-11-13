cfc5445b5cc50dff131d12ea296c84eba3775846
Marine Biologist <marine_science_dude@hotmail.co.uk>
2024-11-13 19:30:57 +0000

    Minecraft TU75 support
    New address set-up, hotbar selection, pause flag found by Clippy95. Hotbar can now be instantly switched to the relevant position with the number keys - this new functionality can be used for other games in future. Inventory-specific bindings have been added - B button has been left at Q for now, shift without click is Y, distribute items (R) cannot currently be set to mouse buttons, E cannot currently be set to close interfaces. Inventory cursor no longer snaps onto item slots.
    
    Co-authored-by: Clippy95 <qpzezoqp15@gmail.com>

--- src/xenia/hid/winkey/hookables/SaintsRow1.cc
+++ src/xenia/hid/winkey/hookables/SaintsRow1.cc
@@ -426,6 +426,7 @@ std::string SaintsRow1Game::ChooseBinds() {
 
   return "Default";
 }
+
 bool SaintsRow1Game::ModifierKeyHandler(uint32_t user_index,
                                         RawInputState& input_state,
                                         X_INPUT_STATE* out_state) {
@@ -450,6 +451,12 @@ bool SaintsRow1Game::ModifierKeyHandler(uint32_t user_index,
   // won't be used
   return true;
 }
+
+void SaintsRow1Game::WeaponSwitchHandler(uint32_t user_index,
+                                         RawInputState& input_state,
+                                         X_INPUT_STATE* out_state, int weapon,
+                                         uint16_t buttons) {}
+
 }  // namespace winkey
 }  // namespace hid
 }  // namespace xe
