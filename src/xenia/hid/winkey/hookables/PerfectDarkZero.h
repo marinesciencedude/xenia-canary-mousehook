147e24bc06b7705ab27d53002537c642ef36cfdf
Clippy95 <qpzezoqp15@gmail.com>
2024-12-14 15:11:01 +0300

    Perfect Dark Zero support (#37)
    * Perfect Dark Zero support
    
    * Add sway
    
    * Add cover camera
    
    * pause flag
    
    * clamp cover
    
    * Change calculation of FOVscaling for FOV patch
    
    * Patch gun sway
    
    * TU3 support
    
    * Right Stick emulation for intro mission
    
    * lint
    
    * Fix pointer crash
    
    * lint
    
    * Read compile date & version instead of build name
    
    * Platinum Hits verison support, modifier & initial bindings
    
    * Different pointer for Platinum Hits and remove address range check for radians X
    
    * Sniper ADS fov fix
    
    * remove pdz_scale_base_fov_sens cvar
    
    * rebase
    
    * Gun sway edits for ADS
    
    * Turret camera support, only tested for TU3
    
    * Local player address for TU3, fixes coop thanks to @A1eNaz for helping me find it
    
    * Port multi base address to TU0 & Plat hits for coop fix
    
    * universal_address chain for isSpecialCam
    
    * Hovercraft cam support
    
    * Jetpac unsupported note
    
    * Emulate LS with mouse when using gadgets.
    
    * Disable mouse LS emulation if SHIFT is held down
    
    * Jetpac cam support
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Clippy95 <lockmare7@gmail.com>

---
+++ src/xenia/hid/winkey/hookables/PerfectDarkZero.h
@@ -0,0 +1,70 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2023 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#ifndef XENIA_HID_WINKEY_PerfectDarkZero_H_
+#define XENIA_HID_WINKEY_PerfectDarkZero_H_
+
+#include "xenia/base/chrono.h"
+#include "xenia/hid/winkey/hookables/hookable_game.h"
+
+namespace xe {
+namespace hid {
+namespace winkey {
+
+class PerfectDarkZeroGame : public HookableGame {
+ public:
+  enum class GameBuild {
+    Unknown,
+    PerfectDarkZero_TU0,
+    PerfectDarkZero_TU3,
+    PerfectDarkZero_PlatinumHitsTU15
+  };
+
+  ~PerfectDarkZeroGame() override;
+
+  bool IsGameSupported();
+
+  float RadianstoDegree(float radians);
+  float DegreetoRadians(float degree);
+
+  bool DoHooks(uint32_t user_index, RawInputState& input_state,
+               X_INPUT_STATE* out_state);
+
+  bool IsPaused(xe::be<uint32_t>* player);
+
+  bool isSpecialCam(xe::be<uint32_t>* player, uint32_t special_cam_flag_offset,
+                    bool universal_addr = false, uint8_t cam_type = -1);
+
+  void HandleRightStickEmulation(RawInputState& input_state,
+                                 X_INPUT_STATE* out_state, bool LSmode = false);
+
+  std::string ChooseBinds();
+
+  bool ModifierKeyHandler(uint32_t user_index, RawInputState& input_state,
+                          X_INPUT_STATE* out_state);
+  void WeaponSwitchHandler(uint32_t user_index, RawInputState& input_state,
+                           X_INPUT_STATE* out_state, int weapon,
+                           uint16_t buttons);
+
+ private:
+  GameBuild game_build_ = GameBuild::Unknown;
+
+  float centering_speed_ = 0.0125f;
+  bool start_centering_ = false;
+  bool disable_sway_ = false;  // temporarily prevents sway being applied
+  std::chrono::steady_clock::time_point last_movement_time_x_;
+  std::chrono::steady_clock::time_point last_movement_time_y_;
+  static xe::be<uint32_t> fovscale_address;
+};
+
+}  // namespace winkey
+}  // namespace hid
+}  // namespace xe
+
+#endif  // XENIA_HID_WINKEY_PerfectDarkZero_H_
