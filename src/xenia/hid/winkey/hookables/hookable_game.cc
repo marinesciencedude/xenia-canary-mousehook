8a72c86a49556deeb6193470fa3d68dfd7722309
Clippy95 <qpzezoqp15@gmail.com>
2025-02-11 19:39:55 +0300

    Implement a wrapper function to display unsupported game version for mousehook. (#67)
    add pos arg and center MESSAGE_TYPE_Host by default

--- src/xenia/hid/winkey/hookables/hookable_game.cc
+++ src/xenia/hid/winkey/hookables/hookable_game.cc
@@ -8,6 +8,7 @@
  */
 
 #include "xenia/hid/winkey/hookables/hookable_game.h"
+#include "xenia/emulator.h"
 #include "xenia/kernel/util/shim_utils.h"
 
 namespace xe {
@@ -30,7 +31,34 @@ xe::be<uint32_t>* multi_pointer(uint32_t base_address,
   }
   return current_pointer;
 }
+#ifdef XENIA_MOUSEHOOK_MESSAGE
+bool mousehook_message_wrapper(std::string message, bool handleseen,
+                               notification_type type, uint8_t pos) {
+  if (mousehook_message_read && handleseen) {
+    return false;
+  }
+  XELOGE(message);
+  const Emulator* emulator = xe::kernel::kernel_state()->emulator();
+  ui::ImGuiDrawer* imgui_drawer = emulator->imgui_drawer();
 
+  if (type == MESSAGE_TYPE_XNotify) {
+    new xe::ui::XNotifyWindow(imgui_drawer, "", message, 0, 2);
+  }
+  if (type == MESSAGE_TYPE_Host) {
+    ui::WindowedAppContext& app_context =
+        emulator->display_window()->app_context();
+    app_context.CallInUIThread([imgui_drawer, message, pos]() {
+      new xe::ui::HostNotificationWindow(imgui_drawer, "ERROR", message, 0,
+                                         pos);
+    });
+  }
+  if (!mousehook_message_read && handleseen) {
+    mousehook_message_read = true;
+  }
+
+  return true;
+}
+#endif
 }  // namespace winkey
 }  // namespace hid
 }  // namespace xe
