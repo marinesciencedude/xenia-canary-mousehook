68d806cc438c0471a06ebe1f60d588256444e524
Clippy95 <qpzezoqp15@gmail.com>
2025-02-11 19:39:55 +0300

    Implement a wrapper function to display unsupported game version for mousehook. (#67)
    add pos arg and center MESSAGE_TYPE_Host by default

--- src/xenia/hid/winkey/hookables/Farcry.cc
+++ src/xenia/hid/winkey/hookables/Farcry.cc
@@ -59,6 +59,24 @@ bool FarCryGame::IsGameSupported() {
     }
   }
 
+#ifdef XENIA_MOUSEHOOK_MESSAGE
+  mousehook_message_wrapper(
+      std::format("MOUSEHOOK: Supported Title ID, but current version '{}' is "
+                  "unsupported. Expected: [{}]",
+                  current_version,
+                  [&]() {
+                    std::string versions;
+                    for (const auto& build : supported_builds) {
+                      if (!versions.empty()) {
+                        versions += ", ";
+                      }
+                      versions += build.second.title_version;
+                    }
+                    return versions;
+                  }()),
+      true);
+#endif
+
   return false;
 }
 
--- src/xenia/hid/winkey/hookables/JustCause.cc
+++ src/xenia/hid/winkey/hookables/JustCause.cc
@@ -60,6 +60,24 @@ bool JustCauseGame::IsGameSupported() {
     }
   }
 
+#ifdef XENIA_MOUSEHOOK_MESSAGE
+  mousehook_message_wrapper(
+      std::format("MOUSEHOOK: Supported Title ID, but current version '{}' is "
+                  "unsupported. Expected: [{}]",
+                  current_version,
+                  [&]() {
+                    std::string versions;
+                    for (const auto& build : supported_builds) {
+                      if (!versions.empty()) {
+                        versions += ", ";
+                      }
+                      versions += build.second.title_version;
+                    }
+                    return versions;
+                  }()),
+      true);
+#endif
+
   return false;
 }
 
