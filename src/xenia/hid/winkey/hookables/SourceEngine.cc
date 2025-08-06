ee02e5587f501b4042405176696a07bc825ff361
Nahedh <qpzezoqp15@gmail.com>
2025-08-06 23:04:12 +0300

    MidHooks (#74)

--- src/xenia/hid/winkey/hookables/SourceEngine.cc
+++ src/xenia/hid/winkey/hookables/SourceEngine.cc
@@ -290,7 +290,9 @@ void SourceEngine::WeaponSwitchHandler(uint32_t user_index,
                                        RawInputState& input_state,
                                        X_INPUT_STATE* out_state, int weapon,
                                        uint16_t buttons) {}
-
+void SourceEngine::MidHookInit() {
+  if (midhook_status == HOOKED) return;
+}
 }  // namespace winkey
 }  // namespace hid
 }  // namespace xe
