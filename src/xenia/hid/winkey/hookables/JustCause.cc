7140ff1b9167741aeafbffe6abad319861b30e8d
Nahedh <qpzezoqp15@gmail.com>
2025-08-06 23:04:12 +0300

    MidHooks (#74)

--- src/xenia/hid/winkey/hookables/JustCause.cc
+++ src/xenia/hid/winkey/hookables/JustCause.cc
@@ -164,7 +164,9 @@ void JustCauseGame::WeaponSwitchHandler(uint32_t user_index,
                                         RawInputState& input_state,
                                         X_INPUT_STATE* out_state, int weapon,
                                         uint16_t buttons) {}
-
+void JustCauseGame::MidHookInit() {
+  if (midhook_status == HOOKED) return;
+}
 }  // namespace winkey
 }  // namespace hid
 }  // namespace xe
