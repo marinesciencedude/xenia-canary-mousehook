d57ce1c3136bcdbad2039029ca4f627516b9900b
Marine Biologist <marine_science_dude@hotmail.co.uk>
2026-03-05 23:33:23 +0000

    Adjustments for newer canary
    removed is_active checks since the callbacks for input_drivers don't work anymore

--- src/xenia/hid/winkey/winkey_input_driver.cc
+++ src/xenia/hid/winkey/winkey_input_driver.cc
@@ -577,7 +577,7 @@ X_RESULT WinKeyInputDriver::GetState(uint32_t user_index,
 
   RawInputState state;
 
-  if (window()->HasFocus() && is_active()) {
+  if (window()->HasFocus()) {
     {
       while (!mouse_events_.empty()) {
         MouseEvent* mouse = &mouse_events_.front();
@@ -869,10 +869,6 @@ void WinKeyInputDriver::WinKeyWindowInputListener::OnRawKeyboard(
 }
 
 void WinKeyInputDriver::OnRawKeyboard(ui::KeyEvent& e) {
-  if (!is_active()) {
-    return;
-  }
-
   const auto key = static_cast<uint16_t>(e.virtual_key());
 
   key_states_[key] = e.prev_state();
@@ -914,10 +910,6 @@ void WinKeyInputDriver::WinKeyWindowInputListener::OnRawMouse(
 }
 
 void WinKeyInputDriver::OnRawMouse(ui::MouseEvent& evt) {
-  if (!is_active()) {
-    return;
-  }
-
   MouseEvent mouse;
   mouse.x_delta = evt.x();
   mouse.y_delta = evt.y();
