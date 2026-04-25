cfedcf6bb1aadf645b268e9ce6f3c39881a47b80
Clippy95 <qpzezoqp15@gmail.com>
2026-04-25 14:50:12 +0300

    Midhook refactor (#94)
    * Midhook refactor 1
    
    * Midhook refactor 2 (remove generic lookup table function)

--- src/xenia/cpu/ppc/ppc_frontend.h
+++ src/xenia/cpu/ppc/ppc_frontend.h
@@ -11,6 +11,7 @@
 #define XENIA_CPU_PPC_PPC_FRONTEND_H_
 
 #include <memory>
+#include <unordered_map>
 
 #include "xenia/base/type_pool.h"
 #include "xenia/cpu/function.h"
@@ -34,7 +35,6 @@ struct PPCBuiltins {
   Function* enter_global_lock;
   Function* leave_global_lock;
   Function* syscall_handler;
-  Function* my_hook;
 };
 
 class PPCFrontend {
@@ -51,14 +51,24 @@ class PPCFrontend {
   bool DeclareFunction(GuestFunction* function);
   bool DefineFunction(GuestFunction* function, uint32_t debug_info_flags);
 
+  // Returns (creating if needed) a per-address builtin for the mid-hook at
+  // the given guest address. The address is baked into arg0 so the handler
+  // never needs to touch scratch.
+  Function* GetOrCreateMidHookBuiltin(uint32_t address);
+
  private:
   Processor* processor_;
   PPCBuiltins builtins_ = {0};
   TypePool<PPCTranslator, PPCFrontend*> translator_pool_;
+  std::unordered_map<uint32_t, Function*> midhook_builtins_;
 };
 // Checks the state of the global lock and sets scratch to the current MSR
 // value.
 void CheckGlobalLock(PPCContext* ppc_context, void* arg0, void* arg1);
+
+using MouseHookMidHook = void (*)(PPCContext* context, void* arg0, void* arg1);
+void RegisterMidHookASM(uint32_t address, MouseHookMidHook hook_function);
+bool HasMidHookAt(uint32_t address);
 }  // namespace ppc
 }  // namespace cpu
 }  // namespace xe
